b8398c9e005a832a1cb9e8ad0021b0e021d70c9e
shohjhahon017 <shohjahonsoilov1@gmail.com>
2024-08-06 23:41:11 +0500

    .

---
+++ js/script.js
@@ -0,0 +1,52 @@
+// let car = {
+//   name: "Mercedes",
+//   year: 2024,
+//   color: "white",
+//   maxSpeed: 350,
+//   weight: 2500,
+//   about: function () {
+//     console.log(this.name + " " + this.year);
+//   },
+// };
+// // Object.freeze(car);
+// // car.color = "black";
+// // console.log(car);
+// car.about();
+
+//1-masala
+// let arr = [1, 2, 3, 6, 9, 2, 8, 7];
+// function oddElement(arg) {
+//   let count = 0;
+//   for (const element of arg) {
+//     if (element % 2 == 0) {
+//       count++;
+//     }
+//   }
+//   return count;
+// }
+// console.log(oddElement(arr));
+
+//2-masala
+// let str = "sal3o6m";
+// function getNumber(arg) {
+//   let res = [];
+//   for (const element of arg) {
+//     if (element > 0) {
+//       res.push(element);
+//     }
+//   }
+
+//   return res;
+// }
+// console.log(getNumber(str));
+
+//3-masala
+let str = "salom dunyo";
+function findWords(arg) {
+  for (const element of arg) {
+  }
+}
+console.log(findWords(str)); //2
+
+//4-masala
+let arr = [1, 3, 6, 9, 7, 5];
